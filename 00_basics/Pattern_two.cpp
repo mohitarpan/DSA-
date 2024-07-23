18f1737ed0bcf3d08e3542428267159261fdd495
Arpan yadav <68770476+mohitarpan@users.noreply.github.com>
2024-07-23 21:54:39 +0000

    new pattern added

---
+++ 00_basics/Pattern_two.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int row,col;
+    for(row =1;row<=5;row=row+1){
+        for(col=1;col<=5;col=col+1){
+            cout<<col<<" ";
+        }
+        cout<<endl;
+    }
+}
+// 1 2 3 4 5 
+// 1 2 3 4 5 
+// 1 2 3 4 5 
+// 1 2 3 4 5 
+// 1 2 3 4 5 
