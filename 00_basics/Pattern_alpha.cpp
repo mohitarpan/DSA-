0cd130e9371dbe3329c2127c4b075872738d0ced
Arpan yadav <68770476+mohitarpan@users.noreply.github.com>
2024-07-24 12:28:15 +0000

    new pattern added with output

--- 00_basics/Pattern_alpha.cpp
+++ 00_basics/Pattern_alpha.cpp
@@ -52,20 +52,62 @@ using namespace std;
 // 11 12 13 14 15 
 // 16 17 18 19 20 
 // 21 22 23 24 25 
-int main() {
-    // int row,col;
-    // int name=1;
-    for(int row =1;row<=5;row=row+1){
-        for(int col=1;col<=5;col=col+1){
-            cout<<(row-1)*5+col<<" ";
-            //  name=name+1;
-        }
+// int main() {
+//     // int row,col;
+//     // int name=1;
+//     for(int row =1;row<=5;row=row+1){
+//         for(int col=1;col<=5;col=col+1){
+//             cout<<(row-1)*5+col<<" ";
+//             //  name=name+1;
+//         }
        
+//         cout<<endl;
+//     }
+// }
+// 1 2 3 4 5 
+// 6 7 8 9 10 
+// 11 12 13 14 15 
+// 16 17 18 19 20 
+// 21 22 23 24 25  without third variable 
+
+// int main() {
+//     int row,col;
+//     for(row =1;row<=5;row=row+1){
+//         for(col=1;col<=row;col=col+1){
+//             cout<<"*"<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
+// * 
+// * * 
+// * * * 
+// * * * * 
+// * * * * * 
+// int main() {
+//     int row,col;
+//     // int count=1;
+//     for(row =1;row<=5;row=row+1){
+//         for(col=1;col<=row;col=col+1){
+//             cout<<col<<" ";
+            
+//         }
+//         cout<<endl;
+//     }
+// }
+// 1 
+// 1 2 
+// 1 2 3 
+// 1 2 3 4 
+// 1 2 3 4 5 
+int main() {
+    int row,col;
+    // int count=1;
+    for(row =1;row<=5;row=row+1){
+        for(col=1;col<=row;col=col+1){
+            cout<<col<<" ";
+            
+        }
         cout<<endl;
     }
 }
-1 2 3 4 5 
-6 7 8 9 10 
-11 12 13 14 15 
-16 17 18 19 20 
-21 22 23 24 25  without third variable 
