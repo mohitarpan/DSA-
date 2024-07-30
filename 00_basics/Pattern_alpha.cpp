a4aa7efee5643189e9d486416e9c80e12c95e7a5
Arpan yadav <68770476+mohitarpan@users.noreply.github.com>
2024-07-30 19:58:36 +0000

    need more pattern

--- 00_basics/Pattern_alpha.cpp
+++ 00_basics/Pattern_alpha.cpp
@@ -100,14 +100,100 @@ using namespace std;
 // 1 2 3 
 // 1 2 3 4 
 // 1 2 3 4 5 
+// int main() {
+//     int row,col;
+//     // int count=1;
+//     for(row =1;row<=5;row=row+1){
+//         for(col=1;col<=row;col=col+1){
+//             cout<<row<<" ";
+            
+//         }
+//         cout<<endl;
+//     }
+// }
+// 1 
+// 2 2 
+// 3 3 3 
+// 4 4 4 4 
+// 5 5 5 5 5 
+// int main() {
+//     int row,col;
+//     // int count=1;
+//     for(row =1;row<=5;row=row+1){
+//         for(col=row;col>=1;col=col-1){
+//             cout<<col<<" ";
+            
+//         }
+//         cout<<endl;
+//     }
+// }
+// 1 
+// 2 1 
+// 3 2 1 
+// 4 3 2 1 
+// 5 4 3 2 1 
+// int main() {
+//     int row,col;
+//     char name='a';
+//     for(row =1;row<=5;row=row+1){
+//         for(col=1;col<=row;col=col+1){
+//             cout<<name<<" ";
+//         }
+//         name=name+1;
+//         cout<<endl;
+//     }
+// }
+// a 
+// b b 
+// c c c 
+// d d d d 
+// e e e e e 
+// int main() {
+//     int row,col;
+//     // int count=1;
+//     for(row =1;row<=5;row=row+1){
+//         for(col=5;col>=row;col=col-1){
+//             cout<<"*"<<" ";
+            
+//         }
+//         cout<<endl;
+//     }
+// }
+// * * * * * 
+// * * * * 
+// * * * 
+// * * 
+// * 
+// int main() {
+//     int row,col;
+//     // int count=1;
+//     for(row =1;row<=5;row=row+1){
+//         for(col=1;col<=5-(row-1);col=col+1){
+//             cout<<col<<" ";
+            
+//         }
+//         cout<<endl;
+//     }
+// }
+// 1 2 3 4 5 
+// 1 2 3 4 
+// 1 2 3 
+// 1 2 
+// 1 
 int main() {
     int row,col;
     // int count=1;
     for(row =1;row<=5;row=row+1){
-        for(col=1;col<=row;col=col+1){
+        for(col=5;col>=5-row+1;col=col-1){
             cout<<col<<" ";
             
         }
         cout<<endl;
     }
 }
+5 
+5 4 
+5 4 3 
+5 4 3 2 
+5 4 3 2 1 
+// hard printing patttern
